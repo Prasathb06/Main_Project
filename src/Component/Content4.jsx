f5123d4d4e689e524a7b358a3a07bf1ef7bb369d
Prasathb06 <arunprasathb045@gmail.com>
2024-07-13 11:05:20 +0530

    file edited

--- src/Component/Content4.jsx
+++ src/Component/Content4.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./Content4.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import img1 from "../images/Logo/TCS.webp";
 import img2 from "../images/Logo/HCL.png";
@@ -8,73 +10,64 @@ import img4 from "../images/Logo/ZOHO1.webp";
 import img5 from "../images/Logo/Amazon.webp";
 import img6 from "../images/Logo/Accenture.webp";
 
-const images = [img1,img2,img3,img4,img5,img6];
-
-const Content4 = () => {
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
+class Content4 extends Component{
+  render(){
+    const Setting = {
+      dots : true,
+      Infinity : true,
+      slidesToShow : 3,
+      slidesToScroll : 1,
+      autoplay : true,
+      speed : 2000,
+      autoPlaySpeed : 2000,
+      cssEase : "linear",
+       responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 3
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToShow: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToShow: 1
         }
       }
     ]
+    };
+    return(
+      <div className='mainContainer'>
+        <h2>Our Student Working at</h2>
+        <Slider {... Setting}>
+          <div className='Container'>
+            <img src={img1}/>
+          </div>
+          <div className='Container'>
+            <img src={img2}/>
+          </div>
+          <div className='Container'>
+            <img src={img3}/>
+          </div>
+          <div className='Container'>
+            <img src={img4}/>
+          </div>
+          <div className='Container'>
+            <img src={img5}/>
+          </div>
+          <div className='Container'>
+            <img src={img6}/>
+          </div>
+        </Slider>
+      </div>
+    )
   };
-  return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <image src={img1}/>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-      </Slider>
-    </div>
-  )
 }
 
-export default Content4
+export default Content4;
