d58138879a0f2a9f285fe61e9f49178a4fc4cbe6
Prasathb06 <arunprasathb045@gmail.com>
2024-07-15 20:49:28 +0530

    new file created and edit file

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -4,6 +4,7 @@ import CounterPage from './CounterPage'
 import Content2 from './Content2'
 import Content3 from './Content3'
 import Content4 from './Content4'
+import Content5 from './Content5'
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <Content2></Content2>
       <Content3></Content3>
       <Content4></Content4>
+      <Content5></Content5>
       <CounterPage></CounterPage>
       
     </div>
