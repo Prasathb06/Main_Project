dfa90a284bf480d0eb67c7ba2d9c8163add01b9d
Prasathb06 <arunprasathb045@gmail.com>
2024-07-16 11:04:55 +0530

    file edited

--- src/Component/Content3.jsx
+++ src/Component/Content3.jsx
@@ -7,7 +7,7 @@ const Content3 = () => {
     <div>
         <Card className='content3_card my-3'>
       <Card.Body className='content3_text'>
-        <h1>Our Vision & Mission</h1>
+        <h1>Vision & Mission</h1>
       <p>This is some text within a card body.</p>
       </Card.Body>
     </Card>
